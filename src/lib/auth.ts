1f1456994a20f4f8e2cbb77f60023bd397da3d97
Tapas Barman <YOUR_GITHUB_EMAIL>
2026-07-26 14:35:22 +0530

    Add environment-aware cookie config for local + production

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -91,5 +91,20 @@ export const authOptions: NextAuthOptions = {
 
         }
     },
+
+    cookies: {
+        sessionToken: {
+            name:
+                process.env.NODE_ENV === "production"
+                    ? `__Secure-next-auth.session-token`
+                    : `next-auth.session-token`,
+            options: {
+                httpOnly: true,
+                sameSite: "lax",
+                path: "/",
+                secure: process.env.NODE_ENV === "production",
+            },
+        },
+    },
 };
 
